30b7b8ff6e956cfec65e3604efe9223ce5ec316c
Visionbike <phuc.visionbike@gmail.com>
2024-07-31 21:54:11 +0800

    update script for downloading datasets in the super-resolution task, including ImageNet, Set5, Set14, BSD100

--- download_sr_datasets.sh
+++ download_sr_datasets.sh
@@ -1,22 +1,23 @@
 FILE=$1
 
-if [ "$FILE" == "SRGAN_ImageNet" ]; then
-  # Download the imagenet dataset and move validation images to labeled subfolders
-  URL="https://huggingface.co/datasets/goodfellowliu/SRGAN_ImageNet/resolve/main/SRGAN_ImageNet.zip"
-  ZIP_FILE=./data/SRGAN_ImageNet.zip
-  mkdir -p ./data
-  wget -N $URL -O $ZIP_FILE
-  unzip $ZIP_FILE -d ./data
-  rm $ZIP_FILE
-elif [ "$FILE" == "Set5" ]; then
-  # Download the Set5 dataset
-  URL="https://huggingface.co/datasets/goodfellowliu/Set5/resolve/main/Set5.zip"
-  ZIP_FILE=./data/Set5.zip
-  mkdir -p ./data/Set5
-  wget -N $URL -O $ZIP_FILE
-  unzip $ZIP_FILE -d ./data/Set5
-  rm $ZIP_FILE
-else
-  echo "Available arguments are SRGAN_ImageNet, Set5"
+DATASETS=("SRGAN_ImageNet" "Set5" "Set14" "BSDS100")
+i=0
+for DATASET in "${DATASETS[@]}"; do
+  if [ "$FILE" == "$DATASET" ]; then
+    # download the selected dataset and move validation images to labeled subfolders
+    URL="https://huggingface.co/datasets/goodfellowliu/$FILE/resolve/main/$FILE.zip"
+    ZIP_FILE=./data/$FILE.zip
+    mkdir -p ./data/$FILE
+    wget -N $URL -O $ZIP_FILE
+    unzip $ZIP_FILE -d ./data/$FILE
+    rm $ZIP_FILE
+    break
+  else
+    i=$((i + 1))
+  fi
+done
+
+if [ "$i" == "${#DATASETS[@]}" ]; then
+  echo "Available arguments are ${DATASETS[*]}"
   exit 1
 fi
